94cd805fa4b66890302145043b3e8a58f46ab9c5
ERRAK Karim <errakkarim@gmail.com>
2017-06-24 18:33:23 +0000

    Quick change

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
  * @author ERRAK Karim <errakkarim@gmail.com>
  */
 return [
-    'site_url' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+    'site_url' => 'http://' . $_SERVER['SERVER_NAME'],
     'project_folder_name' => 'formbuilder',
 ];
